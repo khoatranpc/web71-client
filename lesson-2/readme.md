d103605a311601c4a8fc9d336afa1853b56eb3fa
Tran Khoa <khoatranpc603@gmail.com>
2023-09-29 21:22:41 +0700

    refactor

--- lesson-2/readme.md
+++
@@ -1,4 +0,0 @@
-khởi tạo 1 con server dùng express
-+ API -> các đường dẫn (URL)
-+ METHOD -> phương thức tương tác
-+ 1 số cách gửi dữ liệu lên API: params
--- lesson-3/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import SubjectsRouter from "./subjects.js";
-const RootRouter = Router();
-
-RootRouter.use('/subjects', SubjectsRouter);
-
-export default RootRouter;
